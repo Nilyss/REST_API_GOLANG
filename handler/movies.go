1b4757b486316b52df3a280554e216d7f5c87992
Nicolas DECRESSAC <decressac.nicolas@icloud.com>
2023-03-20 12:01:46 +0100

    Update movies.go
    Adding get a single movie handler

--- handler/movies.go
+++ handler/movies.go
@@ -13,8 +13,8 @@ func TestHandler(res http.ResponseWriter, req *http.Request) {
 
 	// Add the response return message
 	HandlerMessage := []byte(`{
-	"success": true,
-	"message": "The server is running properly"
+		"success": true,
+		"message": "The server is running properly"
 	}`)
 
 	utils.ReturnJsonResponse(res, http.StatusOK, HandlerMessage)
@@ -28,7 +28,7 @@ func GetMovies(res http.ResponseWriter, req *http.Request) {
 		HandlerMessage := []byte(`{
 			"success": false,
 			"message": "Check your HTTP method: Invalid HTTP method executed",
-	}`)
+		}`)
 
 		utils.ReturnJsonResponse(res, http.StatusMethodNotAllowed, HandlerMessage)
 		return
@@ -45,9 +45,64 @@ func GetMovies(res http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		// Add the response return message
 		HandlerMessage := []byte(`{
-		"success": false,
-		"message": "Error parsing the movie data",
-	}`)
+			"success": false,
+			"message": "Error parsing the movie data",
+		}`)
+
+		utils.ReturnJsonResponse(res, http.StatusInternalServerError, HandlerMessage)
+		return
+	}
+
+	utils.ReturnJsonResponse(res, http.StatusOK, movieJSON)
+}
+
+// Get a single movie handler
+func getMovie(res http.ResponseWriter, req *http.Request) {
+
+	if req.Method != "GET" {
+		// Add the response return message
+		HandlerMessage := []byte(`{
+			"success": false,
+			"message": "Check your HTTP method: Invalid HTTP method executed",
+		}`)
+
+		utils.ReturnJsonResponse(res, http.StatusMethodNotAllowed, HandlerMessage)
+		return
+	}
+
+	if _, ok := req.URL.Query()["id"]; !ok {
+		// Add the response return message
+		HandlerMessage := []byte(`{
+			"success": false,
+			"message": "This method requires the movie id",
+		}`)
+		utils.ReturnJsonResponse(res, http.StatusInternalServerError, HandlerMessage)
+		return
+	}
+
+	id := req.URL.Query()["id"][0]
+
+	movie, ok := db.Moviedb[id]
+
+	if !ok {
+		// Add the response return message
+		HandlerMessage := []byte(`{
+			"success": false,
+			"message": "Requested movie not found",
+		}`)
+
+		utils.ReturnJsonResponse(res, http.StatusNotFound, HandlerMessage)
+		return
+	}
+
+	// parse the movie data into json format
+	movieJSON, err := json.Marshal(&movie)
+	if err != nil {
+		// Add the response return message
+		HandlerMessage := []byte(`{
+			"success": false,
+			"message": "Error parsing the movie data",
+		}`)
 
 		utils.ReturnJsonResponse(res, http.StatusInternalServerError, HandlerMessage)
 		return
